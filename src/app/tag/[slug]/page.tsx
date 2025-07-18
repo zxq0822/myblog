406f7f62325de090e3f94cd9a54716b03c580ca5
Qing <141250783+zxq0822@users.noreply.github.com>
2025-07-18 20:25:18 +0800

    Update page.tsx

--- src/app/tag/[slug]/page.tsx
+++ src/app/tag/[slug]/page.tsx
@@ -1,4 +1,3 @@
-// myblog/src/app/tag/[slug]/page.tsx
 import { BlogPostsPreview } from "@/components/BlogPostPreview";
 import { BlogPostsPagination } from "@/components/BlogPostsPagination";
 import { Footer } from "@/components/Footer";
@@ -7,7 +6,6 @@ import { Badge } from "@/components/ui/badge";
 import { wisp } from "@/lib/wisp";
 import { CircleX } from "lucide-react";
 import Link from "next/link";
-import { menuItems } from "@/components/Header"; // 导入导航栏菜单项
 
 interface Params {
   slug: string;
@@ -24,12 +22,8 @@ export async function generateMetadata(
     slug
   } = params;
 
-  // 查找对应的导航栏名称
-  const navItem = menuItems.find(item => item.href === `/tag/${slug}`);
-  const title = navItem ? navItem.name : `#${slug}`;
-
   return {
-    title, // 使用找到的导航栏名称作为页面标题
+    title: `#${slug}`,
     description: `Posts tagged with #${slug}`,
   };
 }
